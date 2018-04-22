821d65e515b54137fa4f1aeb4e1bf0d95c2bb8a3
Garrelt Becker <ga.becker@ostfalia.de>
2018-04-22 01:48:00 -0300

    Add Tilemap and Sources of it

---
+++ ld41_game/assets/data/Brain Machine.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Brain Machine" tilewidth="32" tileheight="64" tilecount="3" columns="3">
+ <image source="../images/Brain_Machine.png" width="96" height="64"/>
+</tileset>
